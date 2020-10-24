5614dcab696d5efcfe9a801d77b83f5f81b04378
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2020-10-24 15:44:57 +0900

    Test

--- samples/tetris/js/sketch.js
+++ samples/tetris/js/sketch.js
@@ -105,10 +105,10 @@ const COLORS = ["#E60012", "#F39800", "#FFF100", "#009944", "#0068B7", "#1D2088"
 
 const ROWS   = 18;
 const COLS   = 10;
-const SIZE   = 16;
 
 let dWidth, dHeight;
-let canvas, ctx, oX, oY, tMng;
+let canvas, ctx, bSize, fSize, oX, oY, tMng;
+let bControll, bRotL, bRotR;
 
 // Window
 window.addEventListener("load", (e)=>{
@@ -122,21 +122,28 @@ function init(){
 	// Window width, height
 	dWidth = document.body.clientWidth;
 	dHeight = document.body.clientHeight;
+	// Block size
+	bSize = dHeight / (ROWS+6);
+	fSize = bSize;
 	// Canvas
 	canvas = document.getElementById("canvas");
 	canvas.width  = dWidth;
 	canvas.height = dHeight;
 	// Context
 	ctx = canvas.getContext("2d");
-	ctx.font        = SIZE + "px Arial";
+	ctx.font        = fSize + "px Arial";
 	ctx.textAlign   = "center";
 	ctx.strokeStyle = "#ffffff";
 	ctx.lineWidth   = 2;
 	// Offset
-	oX = Math.floor(dWidth / 2 - COLS * SIZE / 2);
-	oY = Math.floor(dHeight / 2 - ROWS * SIZE / 2);
+	oX = Math.floor(dWidth/2 - COLS*bSize/2);
+	oY = Math.floor(bSize*1.5);
 	// TetrisManager
 	tMng = new TetrisManager(ROWS, COLS, MINOS, true);
+	// Button
+	bControll = new ButtonControl(dWidth*0.5-bSize*3, dHeight-bSize*3, 32);
+	bRotL = new Button(dWidth*0.5+bSize*1.5, dHeight-bSize*2.5, 32);
+	bRotR = new Button(dWidth*0.5+bSize*4.0, dHeight-bSize*2.5, 32);
 	step();  // Step
 	update();// Update
 }
@@ -153,7 +160,7 @@ function step(){
 		showMsg("Deleted:" + dels + " lines!!");
 		console.log("Deleted:" + dels + " lines!!");
 	}
-	tMng.showConsole(tMng.getData());// Show
+	//tMng.showConsole(tMng.getData());// Show
 	setTimeout(step, 1000);
 }
 
@@ -164,24 +171,37 @@ function update(){
 	ctx.fillStyle = "#333333";
 	ctx.fillRect(0, 0, dWidth, dHeight);
 	ctx.fillStyle = "#444444";
-	ctx.fillRect(oX, oY, COLS*SIZE, ROWS*SIZE);
+	ctx.fillRect(oX, oY, COLS*bSize, ROWS*bSize);
 	// Text
 	ctx.fillStyle = "#cccccc";
-	ctx.fillText("Tetris!!", dWidth/2, dHeight/2-SIZE*ROWS/2-SIZE);
-
+	ctx.fillText("Tetris!!", dWidth/2, bSize);
+	// Tetris
 	let data = tMng.getData();
 	for(let r=0; r<ROWS; r++){
 		for(let c=0; c<COLS; c++){
 			let i = r*COLS + c;
-			let x = oX + c*SIZE;
-			let y = oY + r*SIZE;
+			let x = oX + c*bSize;
+			let y = oY + r*bSize;
 			if(data[i] == 0) continue;
 			ctx.fillStyle = COLORS[data[i]-1];
-			ctx.fillRect(x, y, SIZE-1, SIZE-1);
+			ctx.fillRect(x, y, bSize-1, bSize-1);
 		}
 	}
+	// Button
+	bControll.draw(ctx);
+	bRotL.draw(ctx);
+	bRotR.draw(ctx);
 	setTimeout(update, 100);
 }
+
+document.addEventListener("click", (e)=>{
+	if(tMng.isGameOver()) return;
+	if(bControll.clickLeft(e)) tMng.actionLeft();
+	if(bControll.clickRight(e)) tMng.actionRight();
+	if(bControll.clickDown(e)) tMng.actionDown();
+	if(bRotL.click(e)) tMng.actionRotateL();
+	if(bRotR.click(e)) tMng.actionRotateR();
+});
 
 // Keyboard
 document.addEventListener("keydown", (e)=>{
@@ -205,3 +225,57 @@ document.addEventListener("keydown", (e)=>{
 		//tMng.actionRotateR();
 	}
 });
+
+class ButtonControl{
+
+	constructor(x, y, size){
+		this._x = x;
+		this._y = y;
+		this._size = size*0.5;
+		this._bLeft = new Button(x-size, y, size);
+		this._bRight = new Button(x+size, y, size);
+		this._bDown = new Button(x, y+size, size);
+	}
+
+	clickLeft(e){
+		return this._bLeft.click(e);
+	}
+
+	clickRight(e){
+		return this._bRight.click(e);
+	}
+
+	clickDown(e){
+		return this._bDown.click(e);
+	}
+
+	draw(ctx){
+		this._bLeft.draw(ctx);
+		this._bRight.draw(ctx);
+		this._bDown.draw(ctx);
+	}
+}
+
+class Button{
+
+	constructor(x, y, size){
+		this._x = x - size*0.5;
+		this._y = y - size*0.5;
+		this._size = size;
+	}
+
+	click(e){
+		let cX = e.clientX;
+		let cY = e.clientY;
+		if(cX < this._x) return false;
+		if(this._x + this._size < cX) return false;
+		if(cY < this._y) return false;
+		if(this._y + this._size < cY) return false;
+		return true;
+	}
+
+	draw(ctx){
+		ctx.fillStyle = "#cccccc";
+		ctx.fillRect(this._x, this._y, this._size, this._size);
+	}
+}
