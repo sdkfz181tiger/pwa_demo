2052d0757be477085df0577ae484dc95f93c2420
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2020-11-02 20:39:40 +0900

    ColorTiles

--- samples/colortiles/js/sketch.js
+++ samples/colortiles/js/sketch.js
@@ -3,9 +3,9 @@
 // JavaScript
 
 const T_COLOR = [
-	"#ffffff", "#f44336", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", 
-	"#2196f3", "#03a9f4", "#00bcd4", "#009688", "#4caf50", "#8bc34a", 
-	"#cddc39", "#ffeb3b", "#ffc107", "#ff9800", "#ff5722", "#795548"];
+	"#FFFFFF", "#F44336", "#E91E63", "#9C27B0", "#673Ab7", "#3F51B5", 
+	"#2196F3", "#03A9f4", "#00BCD4", "#009688", "#4CAF50", "#8BC34A", 
+	"#CDDC39", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722", "#795548"];
 
 let grids, tiles;
 let pad, size, corner, sX, sY;
@@ -29,7 +29,6 @@ function draw(){
 function mousePressed(){
 	for(let tile of tiles){
 		if(tile.contains(mouseX, mouseY)){
-			console.log("Tap!!");
 			replaceTiles();
 			return;
 		}
@@ -53,24 +52,49 @@ function replaceTiles(){
 	corner = size * 0.1;
 	sX = width / 2 - pad * grids / 2;
 	sY = height / 2 - pad * grids / 2;
-	// Tiles
+	// Tiles, Color
 	tiles = [];
+	let colCorrect = T_COLOR[Math.floor(grids%T_COLOR.length)];
+	let colIncorrect = getOtherColor(colCorrect);
+	showMsg("o:" + colCorrect + " <-> " + "x:" + colIncorrect);
+	let i = Math.floor(Math.random()*grids**2);
 	for(let r=0; r<grids; r++){
 		for(let c=0; c<grids; c++){
-			tiles.push(new Tile(r, c));
+			if(r*grids+c == i){
+				tiles.push(new Tile(colCorrect, true, r, c));
+			}else{
+				tiles.push(new Tile(colIncorrect, false, r, c));
+			}
 		}
 	}
 }
 
+function getOtherColor(color){
+	let rdm = Math.floor(60-Math.random()*120);
+	let rgb = new RGBColor(color);
+	rgb.r += rdm;
+	rgb.g += rdm;
+	rgb.b += rdm;
+	if(rgb.r < 0)   rgb.r = 0;
+	if(255 < rgb.r) rgb.r = 255;
+	if(rgb.g < 0)   rgb.g = 0;
+	if(255 < rgb.g) rgb.g = 255;
+	if(rgb.b < 0)   rgb.b = 0;
+	if(255 < rgb.b) rgb.b = 255;
+	return rgb.toHex();
+}
+
 class Tile{
 
-	constructor(r, c){
-		this._r   = r;
-		this._c   = c;
-		this._x   = sX + pad * c;
-		this._y   = sY + pad * r;
-		this._dX  = this._x;
-		this._dY  = this._y;
+	constructor(color, correct, r, c){
+		this._color = color;
+		this._correct = correct;
+		this._r  = r;
+		this._c  = c;
+		this._x  = sX + pad * c;
+		this._y  = sY + pad * r;
+		this._dX = this._x;
+		this._dY = this._y;
 	}
 
 	get r(){return this._r;}
@@ -80,10 +104,11 @@ class Tile{
 	set c(n){this._c = c;}
 
 	contains(x, y){
+		if(!this._correct) return false;
 		if(x < this._x) return false;
 		if(y < this._y) return false;
-		if(this._x + this._size < x) return false;
-		if(this._y + this._size < y) return false;
+		if(this._x + size < x) return false;
+		if(this._y + size < y) return false;
 		return true;
 	}
 
@@ -96,9 +121,8 @@ class Tile{
 			this._x += (this._dX - this._x) / 2;
 			this._y += (this._dY - this._y) / 2;
 		}
-		let i = Math.floor(grids%T_COLOR.length);
 		// Background
-		noStroke(); fill(T_COLOR[i]);
+		noStroke(); fill(this._color);
 		square(this._x, this._y, size, size, corner);
 	}
 
